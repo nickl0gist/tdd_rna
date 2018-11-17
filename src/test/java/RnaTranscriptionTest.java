75760ba8b7a017bad50a5cf87f175426f7a40bef
Michał Nowotka <mmmnow@gmail.com>
2018-11-17 21:07:46 +0100

    Update RnaTranscriptionTest.java

--- src/test/java/RnaTranscriptionTest.java
+++ src/test/java/RnaTranscriptionTest.java
@@ -19,24 +19,28 @@ public class RnaTranscriptionTest {
     }
 
 
+    @Ignore
     @Test
     public void testRnaTranscriptionOfGuanineIsCytosine() {
         assertEquals("C", rnaTranscription.transcribe("G"));
     }
 
 
+    @Ignore
     @Test
     public void testRnaTranscriptionOfThymineIsAdenine() {
         assertEquals("A", rnaTranscription.transcribe("T"));
     }
 
 
+    @Ignore
     @Test
     public void testRnaTranscriptionOfAdenineIsUracil() {
         assertEquals("U", rnaTranscription.transcribe("A"));
     }
 
 
+    @Ignore
     @Test
     public void testRnaTranscription() {
         assertEquals("UGCACCAGAAUU", rnaTranscription.transcribe("ACGTGGTCTTAA"));
